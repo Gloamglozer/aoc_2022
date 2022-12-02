e1eb4a6008c3e300b19e97918c77893009f25442
Eric Valentino <evalentino@mail.usf.edu>
2022-12-01 21:07:11 -0500

    day1

---
+++ 1.py
@@ -0,0 +1,10 @@
+# %% Part 1
+with open('1.txt', 'r') as f:
+    text = f.read()
+
+cals = [sum(map(int, elf.split('\n')))
+        for elf in text.split('\n\n')]
+
+max(cals)
+# %% Part 2
+sum(sorted(cals)[-3:])
